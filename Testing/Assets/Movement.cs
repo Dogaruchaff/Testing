c0dd769539788491ef44b6c7b204c9916237c9e6
ChesmeliEgemen <94240071+ChesmeliEgemen@users.noreply.github.com>
2021-11-13 20:26:17 +0300

    Update Movement.cs

--- Testing/Assets/Movement.cs
+++ Testing/Assets/Movement.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     int egemen = 5;
+    int anan = 31;
+   
     // Start is called before the first frame update
     void Start()
     {
