622665c8c371270cdf0634857fe2ca857b20079e
Dogaruchaff <dogaruchaff@hotmail.com>
2021-11-13 20:52:00 +0300

    cvb

--- Testing/Assets/egemen.cs
+++ Testing/Assets/egemen.cs
@@ -5,15 +5,25 @@ using UnityEngine;
 public class egemen : MonoBehaviour
 {
     int haha = 31;
+    bool isprime;
     // Start is called before the first frame update
     void Start()
     {
-        
+        for (int i = 2; i <= 100; i++) {  
+            for (int j = 2; j <= 100; j++) {  
+                if (i != j && i % j == 0) {  
+                    
+                    break;  
+                }  
+            }  
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         
+
+  
     }
 }
