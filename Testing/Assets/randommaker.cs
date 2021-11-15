77a5ad8fa39d2d689e419def29e3b16845b074f9
Dogaruchaff <dogaruchaff@hotmail.com>
2021-11-15 21:30:40 +0300

    random renk degisikligi

--- Testing/Assets/randommaker.cs
+++ Testing/Assets/randommaker.cs
@@ -4,16 +4,22 @@ using UnityEngine;
 
 public class randommaker : MonoBehaviour
 {
-    public GameObject[] Points;
-    public Material[] pointmaterial;
+    public List<GameObject> Points;
+    public List<Material> pointmaterial;
     int sayi;
+    int sayi2;
     void Start()
     {
-        for (int i = 0; i < Points.Length; i++)
+        for (int i = 0; i < 4; i++)
         {
-            sayi = Random.Range(0,Points.Length);
-            
+            sayi = Random.Range(0,Points.Count);
+            sayi2 = Random.Range(0,pointmaterial.Count);
+            Points[sayi].gameObject.GetComponent<MeshRenderer>().material = pointmaterial[sayi2];
+            Points.RemoveAt(sayi);
+            pointmaterial.RemoveAt(sayi2);
+            print(sayi);
         }
+        
     }
 
     void Update()
